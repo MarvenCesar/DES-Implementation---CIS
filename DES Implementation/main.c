c0e294a7922f4b82f72ca853df14a19a125f673a
Marven Cesar <cesarmarven@gmail.com>
2024-02-24 14:21:07 -0500

    Initial Commit

---
+++ DES Implementation/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DES Implementation
+//
+//  Created by Marven Cesar on 2/24/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
