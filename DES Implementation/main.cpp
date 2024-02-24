eac391dd3c20e5d9f4c4901ab76ddc5b14076a39
Marven Cesar <cesarmarven@gmail.com>
2024-02-24 18:05:21 -0500

    Implemented Random Key Generator implemented PC-1

--- DES Implementation/main.cpp
+++ DES Implementation/main.cpp
@@ -1,14 +1,68 @@
 //
-//  main.c
+//  main.cpp
 //  DES Implementation
 //
 //  Created by Marven Cesar on 2/24/24.
 //
 
 #include <iostream>
+#include <string>
+#include <bitset>
+#include <random>
 using namespace std;
+string plaintext;
+
+int PC1[56] = {
+    57, 49, 41, 33, 25, 17, 9,
+    1, 58, 50, 42, 34, 26, 18,
+    10, 2, 59, 51, 43, 35, 27,
+    19, 11, 3, 60, 52, 44, 36,
+    63, 55, 47, 39, 31, 23, 15,
+    7, 62, 54, 46, 38, 30, 22,
+    14, 6, 61, 53, 45, 37, 29,
+    21, 13, 5, 28, 20, 12, 4
+};
+
+bitset<64>generateRandomKey() {
+    // functions from <random> library which helps generate truly random bits which are independent
+    random_device rd; // random number generator
+    mt19937_64 gen(rd()); // seed for 64-bit Mersenne Twister pseudo-random number generator, initialized with a seed obtained from a non-deterministic random number generator
+    uniform_int_distribution<uint64_t> dis; // generate random numbers within a specified range, where each number within the range has an equal probability of being chosen.
+    bitset<64> key(dis(gen)); // generate random 64 bits
+    
+    // set parity bits to ensure odd parity for every 7 bits
+  for (int i = 0; i < 64; i += 8) {
+      bool parity = 0;
+      for (int j = i; j < i+7; ++j ) {
+   parity ^= key[j]; // XOR to calculate parity
+   }
+   key[i + 7] = parity; // Set parity bit to achieve odd parity
+   }
+    
+   return key;
+   }
+
+bitset<56> pc1(const bitset<64>& key){
+    bitset<56> permutedKey;
+    for(int i = 0; i < 56; ++i){
+        permutedKey[55 - i] = key[64-PC1[i]];
+    }
+    return permutedKey;
+}
+
 
 int main() {
+    int bits;
     
-    cout << "Test\n";
+    cout <<"Welcome to the Data Encryption Standard Program\n";
+    cout <<"Please enter the message you would like to encrypte: ";
+    cin >> plaintext;
+    cout <<"Message recieved: " << plaintext<< "\n";
+    bitset<64>j = generateRandomKey();
+    cout << "KEY: " << j;
+    bitset<56>k = pc1(j);
+    cout << "\n After pc-1 KEY: " << k;
+    
+
+    
 };
